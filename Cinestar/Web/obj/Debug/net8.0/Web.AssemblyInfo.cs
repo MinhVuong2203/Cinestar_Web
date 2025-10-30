ff912276bebad71f3009b42cec04b25f6fcf7837
MinhVuong2203 <vuonghihihihi@gmail.com>
2025-10-30 10:58:36 +0700

    test

--- Cinestar/Web/obj/Debug/net8.0/Web.AssemblyInfo.cs
+++ Cinestar/Web/obj/Debug/net8.0/Web.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7aafe63ca3f13cb5c4b75ed7c8c5f5c5f171be63")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6136fc0641c2408b2e5a66deafcd91c1b7847539")]
 [assembly: System.Reflection.AssemblyProductAttribute("Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
