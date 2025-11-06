abcc97e8c19d5242435d247e6ef14b436933e9d1
sHunOrinn <161719997+sHunOrinn@users.noreply.github.com>
2025-11-06 14:18:54 +0700

    sơ sơ nhân viên bán hàng

--- Cinestar/Web/Areas/Admin/Controllers/EmployeeSaleController.cs
+++ Cinestar/Web/Areas/Admin/Controllers/EmployeeSaleController.cs
@@ -1,13 +1,36 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Security.Claims;
+using Microsoft.AspNetCore.Mvc;
+using Web.Areas.Admin.Service;
 
 namespace Web.Areas.Admin.Controllers
 {
+    [Area("Admin")]
     public class EmployeeSaleController : Controller
     {
-        [Area("Admin")]
-        public IActionResult Index()
+        private readonly IEmployeeService _employeeService;
+        public EmployeeSaleController(IEmployeeService employeeService)
         {
-            return View();
+            _employeeService = employeeService;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var employeeIdClaim = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+
+            if (string.IsNullOrEmpty(employeeIdClaim) || !Guid.TryParse(employeeIdClaim, out Guid employeeId))
+            {
+                TempData["Error"] = "Không tìm thấy thông tin nhân viên!";
+                return RedirectToAction("Login", "Account", new { area = "" });
+            }
+
+            // Lấy thông tin employee từ database
+            var employee = await _employeeService.GetEmployeeById(employeeId);
+
+            if (employee == null)
+            {
+                TempData["Error"] = "Nhân viên không tồn tại!";
+                return RedirectToAction("Login", "Account", new { area = "" });
+            }
+            return View(employee);
         }
     }
 }
