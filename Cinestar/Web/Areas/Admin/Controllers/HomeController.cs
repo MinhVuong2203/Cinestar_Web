f13e1a92fabed2971f8e29b8f48e22c8aa757607
Vanhung0611 <vanhungvn741@gmail.com>
2025-11-14 09:07:49 +0700

    thống kê dashboard

--- Cinestar/Web/Areas/Admin/Controllers/HomeController.cs
+++ Cinestar/Web/Areas/Admin/Controllers/HomeController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System.Security.Claims;
+using Web.Areas.Admin.Service;
 
 namespace Web.Areas.Admin.Controllers
 {
@@ -9,10 +10,33 @@ namespace Web.Areas.Admin.Controllers
     public class HomeController : Controller
     {
         //[Authorize(Roles = "Admin, EmployeeSales, ")]
-       
+
+        private readonly IHomeService _homeService;
+
+        public HomeController(IHomeService homeService)
+        {
+            _homeService = homeService;
+        }
         public IActionResult Index()
         {
-            return View();
+            try
+            {
+                ViewBag.TotalMovies = _homeService.GetTotalMovies();
+                ViewBag.NowShowing = _homeService.GetNowShowingMovies();
+                ViewBag.ComingSoon = _homeService.GetComingSoon();
+                ViewBag.AvgDuration = _homeService.GetAverageDuration();
+
+                return View();
+            }
+            catch (Exception ex)
+            {
+                TempData["Error"] = "Có lỗi xảy ra khi tải dữ liệu thống kê!";
+                ViewBag.TotalMovies = 0;
+                ViewBag.NowShowing = 0;
+                ViewBag.ComingSoon = 0;
+                ViewBag.AvgDuration = 0;
+                return View();
+            }
         }
     }
 }
---
+++ Cinestar/Web/Areas/Admin/Service/IHomeService.cs
@@ -0,0 +1,10 @@
+﻿namespace Web.Areas.Admin.Service
+{
+    public interface IHomeService
+    {
+        int GetTotalMovies();
+        int GetNowShowingMovies();
+        int GetComingSoon();
+        double GetAverageDuration();
+    }
+}
